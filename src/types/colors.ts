98dfc6494a28e18113d0c16e1d1a7b06beeac3de
Ryan Alves <ra546038@gmail.com>
2024-09-16 22:07:11 -0300

    feat: type-colors

---
+++ src/types/colors.ts
@@ -0,0 +1,17 @@
+type Colors = {
+    [key: string]: string
+}
+  
+const colors: Colors = {
+    'blue-1': '#204149',
+    'blue-2': '#3E7D8D',
+    'blue-3': '#5DA5B7',
+    'blue-4': '#90C1CE',
+    'blue-5': '#B8CED4',
+    'blue-6': '#D6DBDD',
+    'blue-7': '#E2E8EB',
+    'blue-8': '#F6F6F6',
+    'white': '#FFFFFF',
+}
+  
+export default colors
