e316e33d4b0f429edb7fbdfef39f89ec01365849
Ryan Alves <ra546038@gmail.com>
2024-10-01 06:40:32 -0300

    raw: api connection

--- src/lib/api/fetch.ts
+++ src/lib/api/fetch.ts
@@ -2,7 +2,7 @@ export async function fetchWrapper<T = unknown>(
     input: RequestInfo | URL,
     init?: RequestInit | undefined
 ) {
-    const baseUrl: string = 'https://'
+    const baseUrl: string = 'https://aquarela-nt8n.onrender.com'
     const data: Response = await fetch(`${baseUrl}/${input}`, init)
     const result = await data.json()
     return result as T
