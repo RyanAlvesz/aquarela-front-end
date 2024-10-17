94e05f137bfe9466684e655dc4774e003c9b15d1
Ryan Alves <ra546038@gmail.com>
2024-10-17 12:08:01 -0300

    style: horizontal overflow

--- src/components/ui/PreferencesButtonGrid.tsx
+++ src/components/ui/PreferencesButtonGrid.tsx
@@ -10,22 +10,23 @@ interface preferencesButtonGridProps {
 }
 
 const PreferencesButtonGrid: React.FC<preferencesButtonGridProps> = ({categories}) => {    
-
-    const selectedCategoryIds = useAppSelector((state: RootState) => state.selectedCategories.ids)      
+    
+    const selectedCategoryIds = useAppSelector((state: RootState) => state.selectedCategories.ids)  
 
     return (
-        <div className="grid grid-cols-2 md:grid-flow-col scroll-smooth md:grid-cols-none md:grid-rows-3 gap-[5vw] md:gap-[2.5vh] py-2 rounded-2xl overflow-y-scroll md:overflow-y-hidden md:overscroll-x-scrool">
-            {categories.map((category)=>{
-                const isSelected = selectedCategoryIds.includes(category.id)
-                return (
-                    <PreferencesButton 
-                        key={category.id}
-                        isSelected={isSelected}
-                        selectedCategories={selectedCategoryIds} 
-                        category={category}
-                    />
-                )
-            })}
+        <div 
+            className="preferences-button-grid relative grid grid-cols-2 md:grid-flow-col scroll-smooth md:grid-cols-none md:grid-rows-3 gap-[5vw] md:gap-[2.5vh] py-2 rounded-2xl overflow-y-scroll overflow-x-hidden md:overflow-y-hidden md:overscroll-x-scrool">
+                {categories.map((category)=>{
+                    const isSelected = selectedCategoryIds.includes(category.id)
+                    return (
+                        <PreferencesButton 
+                            key={category.id}
+                            isSelected={isSelected}
+                            selectedCategories={selectedCategoryIds} 
+                            category={category}
+                        />
+                    )
+                })}
         </div>
     )
 }
