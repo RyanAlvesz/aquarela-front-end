e927d6b548bd14253ed1aefa8b5b83537674b455
Ryan Alves <ra546038@gmail.com>
2024-10-29 11:24:40 -0300

    refactor: client and server components

--- src/app/preferences/page.tsx
+++ src/app/preferences/page.tsx
@@ -1,40 +1,15 @@
 import { Metadata } from 'next'
 import Preferences from '@/components/pages/Preferences'
-import { Category } from '@/types'
-import { fetchWrapper } from '@/lib/api/fetch'
-import { useEffect, useState } from 'react'
 
 export const metadata: Metadata = {
   title: 'Preferências',
   description: "Escolha de preferências",
 }
 
-interface GetResp {
-  categorias: Category[]
-}
-
 const Home = () => {
 
-  const [categories, setCategories] = useState<Category[]>([])
-
-  const url = 'v1/aquarela/categories'
-  const options: RequestInit = {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-    cache: 'no-cache',
-  }
-
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const resp = await fetchWrapper<GetResp>(url, options)
-      setCategories(resp.categorias || [])
-    }
-
-    fetchCategories()
-  }, [])
-
   return (
-    <Preferences categories={categories} />
+    <Preferences />
   )
 
 }
--- src/components/pages/Preferences.tsx
+++ src/components/pages/Preferences.tsx
@@ -4,13 +4,33 @@ import { Category } from "@/types";
 import ReduxProvider from "@/store/redux-provider";
 import React from "react";
 import RegisterPreferences from "../ui/forms/RegisterPreferences";
+import { fetchWrapper } from '@/lib/api/fetch'
+import { useEffect, useState } from 'react'
 
-interface preferencesProps {
-    categories: Category[]
+interface GetResp {
+    categorias: Category[]
 }
 
-const Preferences: React.FC<preferencesProps> = ({categories}) => {
+const Preferences: React.FC = () => {
     
+    const [categories, setCategories] = useState<Category[]>([])
+
+    const url = 'v1/aquarela/categories'
+    const options: RequestInit = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        cache: 'no-cache',
+    }
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+        const resp = await fetchWrapper<GetResp>(url, options)
+        setCategories(resp.categorias || [])
+        }
+
+        fetchCategories()
+    }, [])
+
     return (
         <ReduxProvider>
             <RegisterPreferences categories={categories} />
