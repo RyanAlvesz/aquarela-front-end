eeb6d3c87df370f964eb0d5a3c565a08342d46db
RyanAlvesz <ra546038@gmail.com>
2024-11-10 00:20:27 -0300

    style: feed page

--- src/components/ui/feed/DynamicFeed.tsx
+++ src/components/ui/feed/DynamicFeed.tsx
@@ -14,13 +14,15 @@ const DynamicFeed: React.FC<DynamicFeedProps> = ({ feed, infoArea }) => {
 
     <div className="h-fit min-w-fit max-w-full grid bg-blue-7 md:bg-white px-4 pb-[12vh] grid-cols-[repeat(auto-fill,minmax(calc((100vw-4rem)/2),1fr))] md:grid-cols-[repeat(auto-fill,minmax(188px,1fr))] auto-rows-[1px]">
       {feed.map((item) => {
-          return (
-            <FeedItem
-              key={`${item.tipo}-${item.id_publicacao}`}
-              infoArea = {infoArea}
-              item={item}
-            />
-          )
+          if(item.imagens){
+            return (
+              <FeedItem
+                key={`${item.tipo}-${item.id_publicacao}`}
+                infoArea = {infoArea}
+                item={item}
+              />
+            )
+          }
       })}
     </div>
 
