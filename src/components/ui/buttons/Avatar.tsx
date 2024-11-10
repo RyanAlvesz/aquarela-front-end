6834f8a649d23edbfc37804a0c47661fd57978ae
RyanAlvesz <ra546038@gmail.com>
2024-11-10 00:20:48 -0300

    feat: adjusting profile icone

--- src/components/ui/buttons/Avatar.tsx
+++ src/components/ui/buttons/Avatar.tsx
@@ -1,24 +1,38 @@
+'use client'
+
 import Image from "next/image"
 import Link from "next/link"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import standardProfile from "$/public/images/paintings/standard-profile-picture.jpg";
-import { User } from "@/types";
+import { BaseUser } from "@/types";
 
 interface AvatarProps {
-    user: User
+    user: BaseUser
     className?: string,
     nickname: string
 }
 
-const Avatar: React.FC<AvatarProps> = ({user, className, nickname}) => {    
+const Avatar: React.FC<AvatarProps> = ({user, className, nickname}) => {    7
+
+    const [alt, setAlt] = useState('')
+    const [profilePath, setProfilePath] = useState('')
+
+    useEffect(()=>{
+        setProfilePath(nickname)
+    }, [nickname])
+
+    useEffect(() => {
+        setAlt(user.nome)
+    }, [user])
+
     return (
         <Link 
-            href={'/home/profile/' + nickname}
+            href={'/home/profile/' + profilePath}
             className= {`shrink-0 rounded-full flex items-center justify-center aspect-square overflow-hidden ${className}`}
         >
                 <Image 
-                    alt={user.nome} 
+                    alt={alt} 
                     src={user.foto_usuario? user.foto_usuario : standardProfile} 
                 />
         </Link>
