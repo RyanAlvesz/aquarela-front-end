ab44478e2914e2bb546f2d9ff7f8bff65b2c0aba
RyanAlvesz <ra546038@gmail.com>
2024-11-14 12:23:08 -0300

    style: dynamic feed in profile

--- src/app/home/profile/[nickname]/page.tsx
+++ src/app/home/profile/[nickname]/page.tsx
@@ -10,13 +10,14 @@ const Profile = () => {
   const profile = useAppSelector((state: RootState) => state.profile.user as BaseUser & DetailedUser)
   
     return (
-      <div className="relative w-full">
+      <div className="flex items-center justify-center relative w-full">
         {profile.publicacoes == undefined || profile.publicacoes.length == 0? (
           <ProfileMessage message={'Ainda não há nada para mostrar. As publicações ficarão salvas aqui!'}/>
         ) : (
           <DynamicFeed
             feed={profile.publicacoes as (Product | Publication)[]}
             infoArea = {false}
+            className="!bg-transparent md:!w-[60vw] md:!min-w-0"
           />
         )}
       </div>
