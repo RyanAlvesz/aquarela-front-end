a5952603db8fff47f2a3016416d89bdee2c588f6
Ryan Alves <ra546038@gmail.com>
2024-11-19 00:22:32 -0300

    feat: options popover

--- src/components/ui/navigation/DesktopNavBar.tsx
+++ src/components/ui/navigation/DesktopNavBar.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image"
 import Link from "next/link"
-import React, { useState } from "react"
+import React from "react"
 import pallet from "$/public/images/logo/icon.png";
 import bagSVG from "$/public/images/svg/bag.svg";
 import arrowSVG from "$/public/images/svg/arrow.svg";
@@ -12,23 +12,20 @@ import { RootState, useAppSelector } from "@/store/store";
 import DesktopNavBarLink from "./DesktopNavBarLink";
 import ToolTip from "../utils/ToolTip";
 import ConfigModal from "../utils/ConfigModal";
+import Popover, { PopoverContent, PopoverTrigger } from "../utils/Popover";
 
 
 const DesktopNavBar: React.FC = () => {
 
     const user = useAppSelector((state: RootState) => state.user)
-    const [isConfigModalOpen, setIsConfigModalOpen] = useState<boolean>(false)
-    const handleConfigModal = () => {
-        setIsConfigModalOpen(prev => !prev)
-    }
 
-    return(
-        <nav className="hidden z-50 h-[10vh] md:flex items-center justify-between p-[2.5vh] gap-6 fixed top-0 left-0 right-0 bg-white">
+    return (
+        <nav className="hidden z-20 h-[10vh] md:flex items-center justify-between p-[2.5vh] gap-6 fixed top-0 left-0 right-0 bg-white">
             <Link
                 href={'/home/feed'}
                 className="h-full shrink-0"
             >
-                <Image 
+                <Image
                     alt="Aquarela"
                     src={pallet}
                     width={100}
@@ -70,22 +67,26 @@ const DesktopNavBar: React.FC = () => {
                     user={user}
                     className="h-full"
                 />
-                <ToolTip message="Configurações e mais opções">
-                    <button
-                        className="-rotate-90 shrink-0 h-full w-fit"
-                        onClick={handleConfigModal}
-                    >
-                        <Image
-                            alt="Seta de opções"
-                            width={100}
-                            height={100}
-                            src={arrowSVG}
-                        />
-                    </button>
-                </ToolTip>
-                {isConfigModalOpen && (
-                    <ConfigModal />
-                )}
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <div
+                                className={`shrink-0 h-full cursor-pointer w-auto [&>div]:h-full [&>div>div:first-child]:h-full [&[data-state='open']_img]:rotate-90`}
+                            >
+                                <ToolTip message="Configurações e mais opções">
+                                        <Image
+                                            alt="Seta de opções"
+                                            width={100}
+                                            height={100}
+                                            src={arrowSVG}
+                                            className="-rotate-90 w-auto h-full ease-linear duration-500"
+                                        />
+                                </ToolTip>
+                            </div>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <ConfigModal />
+                        </PopoverContent>
+                    </Popover>
             </div>
         </nav>
     )
