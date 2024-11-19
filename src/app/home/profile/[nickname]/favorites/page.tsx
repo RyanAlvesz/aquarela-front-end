10ad857bd4fcf00fe8777ed38be808944b6178cb
Ryan Alves <ra546038@gmail.com>
2024-11-19 00:32:11 -0300

    feat: user favorite items

--- src/app/home/profile/[nickname]/favorites/page.tsx
+++ src/app/home/profile/[nickname]/favorites/page.tsx
@@ -1,9 +1,71 @@
+'use client'
+
+import DynamicFeed from "@/components/ui/feed/DynamicFeed"
+import ProfileMessage from "@/components/ui/text/ProfileMessage"
+import { fetchWrapper } from "@/lib/api/fetch"
+import { RootState, useAppSelector } from "@/store/store"
+import { DetailedProduct, DetailedPublication } from "@/types"
+import { useParams, useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+
+interface GetResp {
+  itens: (DetailedProduct | DetailedPublication) []
+  status: boolean
+}
+
 const ProfileFavorites = () => {
-    return (
-      <main>
-      </main>
-    )
+
+  const currentUser = useAppSelector((state: RootState) => state.user)
+  const [favoriteItens, setFavoriteItens] = useState<(DetailedProduct | DetailedPublication) []>([])
+  const params = useParams()
+  const router = useRouter()
+
+  useEffect(() => {
+    const noCurrentUser = () => {
+      if (currentUser.nome_usuario !== params.nickname) {
+        router.back()
+      }
+    }
+    noCurrentUser()
+  }, [currentUser.nome_usuario, params.nickname, router])
+
+  useEffect(() => {
+
+    const url = 'v1/aquarela/favorite/user/' + currentUser.id
+    console.log(url);
+    
+
+    const options: RequestInit = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      cache: 'no-cache',
+    }
+
+    const fetchFeedItems = async () => {
+      const resp = await fetchWrapper<GetResp>(url, options)
+      if (resp.itens.length > 0) {
+        setFavoriteItens(resp.itens)        
+      }
+    }
+
+    fetchFeedItems()
+
+  }, [currentUser])
+
+  return (
+    <main className="flex items-center justify-center relative w-full">
+      {favoriteItens.length < 0 ? (
+        <ProfileMessage message={'Sua lista de favoritos está vazia! Comece a salvar o que mais gosta'} />
+      ) : (
+        <DynamicFeed
+          feed={favoriteItens}
+          infoArea={"favorite"}
+          className="!bg-transparent md:!w-[60vw] md:!min-w-0"
+        />
+      )}
+    </main>
+  )
+
 }
 
 export default ProfileFavorites
-  
