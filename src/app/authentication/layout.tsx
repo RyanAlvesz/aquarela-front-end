cc5d66fc5e98f6aa716ed6337392bccbb4175091
Ryan Alves <ra546038@gmail.com>
2024-11-19 00:18:12 -0300

    fix: syntax error

--- src/app/authentication/layout.tsx
+++ src/app/authentication/layout.tsx
@@ -5,7 +5,7 @@ import ReturnButton from "@/components/ui/buttons/ReturnButton";
 
 const VeniceLayout = ({children}: {children: React.ReactNode}) => {
 
-    const setReturnButtonColorByWindowWidht = () => {
+    const SetReturnButtonColorByWindowWidht = () => {
         const { width } = useWindowDimensions()
         if(width && width < 768)
             return 'blue'
@@ -16,7 +16,7 @@ const VeniceLayout = ({children}: {children: React.ReactNode}) => {
     return (
         <div className="w-full min-h-screen grid grid-rows-[auto_1fr] md:grid-cols-[12fr_11fr] md:grid-rows-1 p-4 md:p-0 gap-4 bg-blue-7 md:bg-venice-bridge bg-no-repeat bg-cover md:bg-right bg-fixed">
             <header className="flex items-center justify-start w-full h-fit md:p-9">
-                <ReturnButton width={6} color={setReturnButtonColorByWindowWidht()}/>
+                <ReturnButton width={6} color={SetReturnButtonColorByWindowWidht()}/>
             </header>
             {children}
         </div>
