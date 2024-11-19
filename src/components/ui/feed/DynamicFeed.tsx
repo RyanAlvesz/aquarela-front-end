51c6564c06947b4ff0f110cfd33040db3670ec40
Ryan Alves <ra546038@gmail.com>
2024-11-19 00:27:15 -0300

    style: feed item size

--- src/components/ui/feed/DynamicFeed.tsx
+++ src/components/ui/feed/DynamicFeed.tsx
@@ -12,7 +12,7 @@ const DynamicFeed: React.FC<DynamicFeedProps> = ({ feed, infoArea, className }) 
   const feedItemsWithImages = feed.filter(item => item.imagens && item.imagens.length > 0)
 
   return (
-    <div className={"h-fit min-w-fit max-w-full grid bg-blue-7 md:bg-white px-4 pb-[12vh] grid-cols-[repeat(auto-fill,minmax(calc((100vw-4rem)/2),1fr))] md:grid-cols-[repeat(auto-fill,minmax(188px,1fr))] auto-rows-[1px] " + className}>
+    <div className={"h-fit min-w-fit max-w-full grid bg-blue-7 md:bg-white px-4 pb-[12vh] grid-cols-[repeat(auto-fill,minmax(calc((100vw-4rem)/2),1fr))] md:grid-cols-[repeat(auto-fill,minmax(270px,1fr))] auto-rows-[1px] " + className}>
       {feedItemsWithImages.map((item) => {
           return (
             <FeedItem
