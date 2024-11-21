f0b7dd39aa559a43ccb99290d38d6de4011cf15e
Ryan Alves <ra546038@gmail.com>
2024-11-21 01:48:18 -0300

    feat: new classname prop

--- src/components/ui/inputs/ConfigInput.tsx
+++ src/components/ui/inputs/ConfigInput.tsx
@@ -8,10 +8,11 @@ interface ConfigInputProps {
     label: string
     required?: boolean
     maxLength?: number
+    className?: string
     onChange: (newValue: string) => void
 }
 
-const ConfigInput: React.FC<ConfigInputProps> = ({label, type, onChange, value, required, maxLength}) => {
+const ConfigInput: React.FC<ConfigInputProps> = ({label, type, onChange, value, required, maxLength, className}) => {
     
     const [inputValue, setInputValue] = useState(value)
 
@@ -24,7 +25,7 @@ const ConfigInput: React.FC<ConfigInputProps> = ({label, type, onChange, value, 
       }, [value]);
     
     return(
-        <div className="flex flex-col gap-1 w-full">
+        <div className={"flex flex-col gap-1 w-full " + className}>
             <label htmlFor={label} className="font-medium text-blue-1 text-[1.15rem]">{label}</label>
             <input onChange={handleChange} value={inputValue} maxLength={maxLength} required={required} type={type} id={label} name={label} className="p-2 md:px-3 h-[7vh] md:h-12 rounded-md text-blue-1 bg-blue-5/90"/>
         </div>
