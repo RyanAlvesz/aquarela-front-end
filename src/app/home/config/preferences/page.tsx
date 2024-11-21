f4482d0759dd1fae1300d305f971d6eb7e0ddad4
Ryan Alves <ra546038@gmail.com>
2024-11-21 01:45:16 -0300

    feat: dark mode

--- src/app/home/config/preferences/page.tsx
+++ src/app/home/config/preferences/page.tsx
@@ -2,15 +2,31 @@
 
 import ConfigTitle from "@/components/ui/buttons/ConfigTitle"
 import PreferencesInput from "@/components/ui/inputs/PreferencesInput"
-import { RootState, useAppSelector } from "@/store/store"
+import { setDarkMode } from "@/store/darkModeSlice"
+import { RootState, useAppDispatch, useAppSelector } from "@/store/store"
+import { useEffect } from "react"
 
 const ConfigPreferences = () => {
 
   const user = useAppSelector((state: RootState) => state.user)
+  const darkMode = useAppSelector((state: RootState) => state.darkMode.darkMode)
+  const dispatch = useAppDispatch()
+
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (darkMode) {
+      root.classList.add('dark');
+    } else {
+      root.classList.remove('dark');
+    }
+  }, [darkMode])
 
   const handleAvailability = () => {}
   const handleComments = () => {}
-  const handleDarkMode = () => {}
+  const handleDarkMode = () => {
+    dispatch(setDarkMode(!darkMode))
+  }
 
     return (
       <main className="bg-blue-7 min-h-screen flex justify-center p-4 md:py-8 md:bg-transparent md:h-fit md:grow">
@@ -19,7 +35,7 @@ const ConfigPreferences = () => {
           <div className="flex flex-col gap-3 md:gap-4 w-full md:p-0">
             <PreferencesInput label="Posso receber pedidos de arte" onChange={handleAvailability} initialState={user.disponibilidade as boolean} />
             <PreferencesInput label="Permitir comentários" onChange={handleComments} initialState={false} />
-            <PreferencesInput label="Modo escuro" onChange={handleDarkMode} initialState={false} />
+            <PreferencesInput label="Modo escuro" onChange={handleDarkMode} initialState={darkMode} />
           </div>
         </div>
       </main>
