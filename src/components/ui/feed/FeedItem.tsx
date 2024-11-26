40d258c5657694beb71d25362fdbb04307ceca8b
RyanAlvesz <ra546038@gmail.com>
2024-11-26 00:51:27 -0300

    feat: create and update folder

--- src/components/ui/feed/FeedItem.tsx
+++ src/components/ui/feed/FeedItem.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image"
 import Avatar from "../buttons/Avatar"
 import React, { useEffect, useState } from "react"
-import { DetailedProduct, DetailedPublication, Product, Publication } from "@/types"
+import { DetailedProduct, DetailedPublication, Product, Publication, Folder as IFolder } from "@/types"
 import emptyHeartSVG from "$/public/images/svg/empty-heart.svg"
 import filledHeartSVG from "$/public/images/svg/filled-heart.svg"
 import emptyFavoriteSVG from "$/public/images/svg/empty-favorite.svg"
@@ -11,42 +11,55 @@ import filledFavoriteSVG from "$/public/images/svg/filled-favorite.svg"
 import arrowSVG from "$/public/images/svg/arrow.svg"
 import watermark from "$/public/images/logo/watermark.png"
 import optionsSVG from "$/public/images/svg/options.svg"
+import deleteSVG from "$/public/images/svg/plus.svg"
 import shareSVG from "$/public/images/svg/share.svg"
 import useWindowDimensions from "@/hooks/useWindowDimension"
 import { useRouter } from "next/navigation"
 import { fetchWrapper } from "@/lib/api/fetch"
 import { RootState, useAppSelector } from "@/store/store"
-import alert from "@/types/alert"
+import alert, { loader, stopLoader } from "@/types/alert"
 import Popover, { PopoverContent, PopoverTrigger } from "../utils/Popover"
 import ItemModal from "./ItemModal"
+import ConfigInput from "../inputs/ConfigInput"
+import GradientButton from "../buttons/GradientButton"
+import ToolTip from "../utils/ToolTip"
 
 interface FeedItemProps {
     item: Product | DetailedProduct | Publication | DetailedPublication
     infoArea: 'like' | 'favorite' | false
     itemSize?: (arg: number) => number
+    deleteFolder?: IFolder
+    refreshItems?: () => void
 }
 
 const isDetailed = (item: Product | DetailedProduct | Publication | DetailedPublication): item is DetailedProduct | DetailedPublication => {
     return 'id_dono_publicacao' in item;
 }
 
-const FeedItem: React.FC<FeedItemProps> = ({ item, infoArea, itemSize }) => {
+const FeedItem: React.FC<FeedItemProps> = ({ item, infoArea, itemSize, deleteFolder, refreshItems }) => {
 
     const [isLiked, setIsLiked] = useState<boolean>(Number(item.curtida) === 1)
     const [isFavorited, setIsFavorited] = useState<boolean>(Number(item.favorito) === 1)
-    const [isOptionsOpen, setOptionsOpen] = useState(false);
-    const [isHovered, setIsHovered] = useState(false);
+    const [isOptionsOpen, setOptionsOpen] = useState(false)
+    const [isHovered, setIsHovered] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
     const currentUser = useAppSelector((state: RootState) => state.user)
+    const [isCreateFolderButton, setIsCreateFolderButton] = useState<boolean>(false)
+    const [folderName, setFolderName] = useState<string>('')
+    const [loading, setLoading] = useState(false)
     const hasMultipleImages = Boolean(item.imagens.length > 1)
     const windowWidth = useWindowDimensions().width
     const [imageDimensions, setImageDimensions] = useState<{ width: number; height: number } | null>(null);
-    const baseHeight = infoArea ? 46 : 6
-    const itemWidth = itemSize? itemSize(windowWidth as number) : (windowWidth as number - 32)/5
-    console.log(itemWidth);
-    
-    
+    let baseHeight = infoArea ? 46 : 18
+    const itemWidth = itemSize ? itemSize(windowWidth as number) : (windowWidth as number - 32) / 5
+
     const router = useRouter()
+
+    useEffect(() => {
+        if (loading) {
+            loader()
+        }
+    }, [loading])
 
     const handleProductLike = async () => {
 
@@ -188,6 +201,7 @@ const FeedItem: React.FC<FeedItemProps> = ({ item, infoArea, itemSize }) => {
                     height = Math.max(baseHeight, ((imgHeight * itemWidth) / imgWidth) + baseHeight);
                 }
             } else {
+                baseHeight += 8
                 const divWidth = ((windowWidth as number - 4 * 16) / 2)
                 height = Math.max(baseHeight, ((imgHeight * divWidth) / imgWidth) + baseHeight)
             }
@@ -195,29 +209,111 @@ const FeedItem: React.FC<FeedItemProps> = ({ item, infoArea, itemSize }) => {
         return Math.ceil(height)
     }
 
+    const createFolder = async () => {
+
+        if (folderName != '') {
+
+            setLoading(true)
+
+            const url: string = 'v1/aquarela/folder'
+
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    nome: folderName,
+                    id_usuario: currentUser.id
+                }),
+            }
+
+            interface getResp {
+                pasta: IFolder,
+                status_code: number
+                status: string
+            }
+
+            const resp = await fetchWrapper<getResp>(url, options)
+
+            if (resp && resp.status_code == 201) {
+                stopLoader()
+                setIsCreateFolderButton(false)
+                setFolderName('')
+            }
+
+        }
+
+    }
+
     const handleItemClick = () => {
         item.tipo == 'postagem' ? router.push('/home/publication/' + item.id_publicacao) : router.push('/home/product/' + item.id_publicacao)
     }
 
+    const handleDeleteItemFolder = async() => {
+        
+        let resp
+
+        if (item.tipo == 'produto') {
+            const url: string = 'v1/aquarela/folders/products'
+            const options: RequestInit = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id_produto: item.id_publicacao,
+                    id_pasta: deleteFolder?.id_pasta
+                })
+            }
+            resp = await fetchWrapper(url, options)
+        } else {
+            const url: string = 'v1/aquarela/folders/posts'
+            const options: RequestInit = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id_postagem: item.id_publicacao,
+                    id_pasta: deleteFolder?.id_pasta
+                })
+            }
+            resp = await fetchWrapper(url, options)
+        }
+
+        if (resp) {            
+
+            alert({
+                icon: 'success',
+                title: 'Item removido com sucesso'
+            })
+
+            if (refreshItems) {
+                refreshItems()
+            }
+
+        }
+
+    }
+
     return (
         <>
-
-            <div 
-                className={`flex-col gap-2 p-2 h-fit ${isLoading? 'flex' : 'hidden'}`}
-                style={{width: itemWidth+'px'}}
+            <div
+                className={`flex-col gap-2 p-2 h-fit ${isLoading ? 'flex' : 'hidden'}`}
+                style={{ width: itemWidth + 'px' }}
             >
-                <div 
+                <div
                     className="w-full bg-blue-6/70 animate-pulse rounded-xl"
-                    style={{height: itemWidth+'px'}}
+                    style={{ height: itemWidth + 'px' }}
                 />
-                <div 
+                <div
                     className="w-full bg-blue-6/70 animate-pulse rounded-xl"
-                    style={{height: baseHeight+'px'}}
+                    style={{ height: baseHeight + 'px' }}
                 />
             </div>
-
             <div
-                className={`flex-col gap-2 p-2 cursor-pointer ${isLoading? 'hidden' : 'flex'}`}
+                className={`flex-col gap-2 p-2 cursor-pointer ${isLoading ? 'hidden' : 'flex'}`}
                 style={{
                     gridRowEnd: `span ${calculateGridRowEnd()}`,
                     minHeight: '2.5rem',
@@ -238,11 +334,11 @@ const FeedItem: React.FC<FeedItemProps> = ({ item, infoArea, itemSize }) => {
                         width={500}
                         height={500}
                         onClick={handleItemClick}
-                        className="w-full h-auto object-cover shadow-feed-item rounded-xl"
+                        className="w-full h-auto object-cover shadow-feed-item rounded-md md:rounded-xl"
                     />
                     {Boolean(Number(item.marca_dagua)) && (
                         <div
-                            className={`overflow-hidden rounded-xl absolute inset-0 pointer-events-none`}
+                            className={`overflow-hidden rounded-md md:rounded-xl absolute inset-0 pointer-events-none`}
                         >
                             <div
                                 className="absolute bg-cover inset-0  w-[165%] h-[165%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -rotate-12"
@@ -254,7 +350,7 @@ const FeedItem: React.FC<FeedItemProps> = ({ item, infoArea, itemSize }) => {
                     )}
                     {isHovered && (
                         <div
-                            className="hidden md:flex items-end overflow-hidden p-2 rounded-xl absolute inset-0 bg-black/40 pointer-events-none"
+                            className="hidden md:flex items-end overflow-hidden p-2 ease-linear animate-fade rounded-md md:rounded-xl absolute inset-0 bg-black/40 pointer-events-none"
                         >
                             {hasMultipleImages && (
                                 <div className="absolute left-0 right-0 top-1/2 -translate-y-1/2 flex justify-between p-2 pointer-events-auto">
@@ -285,6 +381,22 @@ const FeedItem: React.FC<FeedItemProps> = ({ item, infoArea, itemSize }) => {
                                 </div>
                             )}
                             <div className="flex w-full justify-end gap-3 pointer-events-auto">
+                                {deleteFolder && (
+                                    <ToolTip message="Remover da pasta">
+                                        <button
+                                            className="bg-blue-1 rounded-full w-8 h-8 flex items-center justify-center"
+                                            onClick={handleDeleteItemFolder}
+                                        >
+                                            <Image
+                                                alt="Remover"
+                                                src={deleteSVG}
+                                                width={100}
+                                                height={100}
+                                                className="w-[55%] h-auto rotate-45"
+                                            />
+                                        </button>
+                                    </ToolTip>
+                                )}
                                 <button
                                     className="bg-white rounded-full w-8 h-8 flex items-center justify-center"
                                     onClick={handleShare}
@@ -317,10 +429,15 @@ const FeedItem: React.FC<FeedItemProps> = ({ item, infoArea, itemSize }) => {
                                         </button>
                                     </PopoverTrigger>
                                     <PopoverContent>
-                                        <ItemModal
-                                            item={item}
-                                            onFavorite={item.tipo === 'produto' ? handleProductFavorite : handlePublicationFavorite}
-                                        />
+                                        {
+                                            !isCreateFolderButton && (
+                                                <ItemModal
+                                                    createFolder={setIsCreateFolderButton}
+                                                    item={item}
+                                                    onFavorite={item.tipo === 'produto' ? handleProductFavorite : handlePublicationFavorite}
+                                                />
+                                            )
+                                        }
                                     </PopoverContent>
                                 </Popover>
                             </div>
@@ -341,13 +458,17 @@ const FeedItem: React.FC<FeedItemProps> = ({ item, infoArea, itemSize }) => {
                                     <button className="h-8 w-8" onClick={handleProductLike}>
                                         <Image
                                             alt='Curtida'
+                                            width={100}
+                                            height={100}
                                             src={isLiked == true ? filledHeartSVG : emptyHeartSVG}
                                         />
                                     </button>
                                 ) : (
                                     <button className="h-8 w-8" onClick={handleProductFavorite}>
                                         <Image
-                                            alt='Curtida'
+                                            alt='Favorito'
+                                            width={100}
+                                            height={100}
                                             src={isFavorited == true ? filledFavoriteSVG : emptyFavoriteSVG}
                                         />
                                     </button>
@@ -367,13 +488,17 @@ const FeedItem: React.FC<FeedItemProps> = ({ item, infoArea, itemSize }) => {
                                     <button className="h-8 w-8" onClick={handlePublicationLike}>
                                         <Image
                                             alt='Curtida'
+                                            height={100}
+                                            width={100}
                                             src={isLiked == true ? filledHeartSVG : emptyHeartSVG}
                                         />
                                     </button>
                                 ) : (
                                     <button className="h-8 w-8" onClick={handlePublicationFavorite}>
                                         <Image
-                                            alt='Curtida'
+                                            alt='Favorito'
+                                            height={100}
+                                            width={100}
                                             src={isFavorited == true ? filledFavoriteSVG : emptyFavoriteSVG}
                                         />
                                     </button>
@@ -384,7 +509,21 @@ const FeedItem: React.FC<FeedItemProps> = ({ item, infoArea, itemSize }) => {
                     </div>
                 )}
             </div>
-
+            {isCreateFolderButton && (
+                <div className="fixed top-0 left-0 flex animate-fade animate-duration-1000 animate-ease-in-out items-center justify-center h-screen w-screen z-[999] bg-black/30"
+                    onClick={() => setIsCreateFolderButton(false)}
+                >
+                    <form
+                        onClick={(e) => e.stopPropagation()}
+                        onSubmit={(e) => e.preventDefault()}
+                        className="bg-blue-7 w-[90vw] md:w-[35vw] rounded-2xl flex flex-col items-center h-fit p-8"
+                    >
+                        <h2 className="text-2xl md:text-3xl text-blue-1 font-bold mb-6">Criar pasta</h2>
+                        <ConfigInput label="Nome" onChange={setFolderName} type="text" value={folderName} className="md:[&>label]:text-2xl md:[&>input]:h-12" maxLength={100} required />
+                        <GradientButton direction="bottom" label="Salvar" primaryColor={"blue-1"} secundaryColor={"blue-1"} className="w-full md:w-1/2 [&>p]:!text-lg md:[&>p]:!text-2xl p-2 mt-4" onClick={createFolder} />
+                    </form>
+                </div>
+            )}
         </>
     )
 }
--- src/components/ui/feed/ItemModal.tsx
+++ src/components/ui/feed/ItemModal.tsx
@@ -1,24 +1,107 @@
 'use client'
 
-import { DetailedProduct, Product, Publication } from "@/types"
-import React, { useState } from "react"
+import { fetchWrapper } from "@/lib/api/fetch"
+import { RootState, useAppSelector } from "@/store/store"
+import { DetailedProduct, Folder, Product, Publication } from "@/types"
+import Image from "next/image"
+import React, { useEffect, useState } from "react"
+import addSVG from '$/public/images/svg/plus.svg'
+import Link from "next/link"
+import alert from "@/types/alert"
 
 interface ItemModalProps {
     item: Product | DetailedProduct | Publication | DetailedProduct
     onFavorite: () => Promise<boolean>
+    createFolder: (v: boolean) => void
 }
 
-const ItemModal: React.FC<ItemModalProps> = ({item, onFavorite}) => {
-    
-    const [favorite, setFavorite] = useState(Boolean(Number(item.favorito)))
+interface FoldersResp {
+    status: boolean
+    status_code: number
+    message: string
+    pastas: Folder[]
+}
 
-    const handleFavorite = async() => {
+const ItemModal: React.FC<ItemModalProps> = ({ item, onFavorite, createFolder }) => {
+
+    const [favorite, setFavorite] = useState(Boolean(Number(item.favorito)))
+    const [saveItem, setSaveItem] = useState<boolean>(false)
+    const [folders, setFolders] = useState<Folder[]>([])
+    const currentUser = useAppSelector((state: RootState) => state.user)
+
+    const handleFavorite = async () => {
         const resp = await onFavorite()
         setFavorite(resp)
     }
 
-    const handleDownload = async() => {
-        
+    const fetchFolders = async () => {
+        const url: string = `v1/aquarela/folders/user/` + currentUser.id
+
+        const options: RequestInit = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            cache: 'no-cache',
+        }
+
+        const resp = await fetchWrapper<FoldersResp>(url, options)
+        if (resp) {
+            setFolders(resp.pastas)
+        }
+
+    }
+
+    const handleSaveItem = () => {
+        setSaveItem(true)
+    }
+
+    const postSaveItem = async (folder: Folder) => {
+
+        let resp
+
+        if (item.tipo == 'produto') {
+            const url: string = 'v1/aquarela/folders/products'
+            const options: RequestInit = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id_produto: item.id_publicacao,
+                    id_pasta: folder.id_pasta
+                })
+            }
+            resp = await fetchWrapper(url, options)
+        } else {
+            const url: string = 'v1/aquarela/folders/posts'
+            const options: RequestInit = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id_postagem: item.id_publicacao,
+                    id_pasta: folder.id_pasta
+                })
+            }
+            resp = await fetchWrapper(url, options)
+        }
+
+        if (resp) {
+
+            alert({
+                icon: 'success',
+                title: 'Item adicionado com sucesso'
+            })
+        }
+
+    }
+
+    useEffect(() => {
+        fetchFolders()
+    })
+
+    const handleDownload = async () => {
+
         for (let i = 0; i < item.imagens.length; i++) {
             const image = item.imagens[i]
             try {
@@ -27,25 +110,68 @@ const ItemModal: React.FC<ItemModalProps> = ({item, onFavorite}) => {
                 const link = document.createElement('a')
                 const url = URL.createObjectURL(blob)
                 link.href = url
-                link.download = `${item.nome}_${i + 1}.jpg` 
+                link.download = `${item.nome}_${i + 1}.jpg`
                 document.body.appendChild(link)
                 link.click()
-                document.body.removeChild(link)            
+                document.body.removeChild(link)
                 URL.revokeObjectURL(url)
             } catch (error) {
                 console.error(`Erro ao baixar a imagem ${i + 1}:`, error)
             }
         }
-    
+
     }
 
     return (
-        <div className="w-[15vw] z-40 relative h-fit py-4 px-2 animate-fade-down animate-duration-1000 animate-ease-in-out flex flex-col gap-6 bg-blue-8 rounded-xl shadow-[0_0_8px_0px_rgba(0,0,0,0.2)] right-0">
-                <div className="flex flex-col gap-1 items-start text-start text-blue-1 font-medium">
-                    <button onClick={handleFavorite} className="text-start hover:bg-blue-2/20 ease-linear duration-150 w-full p-1 px-2 rounded-md"> {favorite? 'Desfavoritar' : 'Favoritar'} </button>
-                    <button onClick={handleDownload} className="text-start hover:bg-blue-2/20 ease-linear duration-150 w-full p-1 px-2 rounded-md">Baixar imagem</button>
-                    <button onClick={() => null} className="text-start text-blue-2 hover:bg-blue-2/20 ease-linear duration-150 w-full p-1 px-2 rounded-md">Salvar</button>
+        <div className="relative">
+            {!saveItem ? (
+                <div className="w-[65vw] md:w-[15vw] z-40 relative h-fit py-4 px-2 animate-fade-down animate-duration-1000 animate-ease-in-out flex flex-col gap-6 bg-blue-8 rounded-xl shadow-[0_0_8px_0px_rgba(0,0,0,0.2)] right-0">
+                    <div className="flex flex-col gap-1 items-start text-start text-blue-1 font-medium">
+                        <button onClick={handleFavorite} className="text-start hover:bg-blue-2/20 ease-linear duration-150 w-full p-1 px-2 rounded-md"> {favorite ? 'Desfavoritar' : 'Favoritar'} </button>
+                        <button onClick={handleDownload} className="text-start hover:bg-blue-2/20 ease-linear duration-150 w-full p-1 px-2 rounded-md">Baixar imagem</button>
+                        <button onClick={handleSaveItem} className="text-start text-blue-2 hover:bg-blue-2/20 ease-linear duration-150 w-full p-1 px-2 rounded-md">Salvar</button>
+                    </div>
                 </div>
+            ) : (
+                <div className="w-[85vw] md:w-[25vw] z-50 relative h-fit animate-fade animate-duration-1000 animate-ease-in-out flex flex-col bg-blue-8 rounded-xl shadow-[0_0_8px_0px_rgba(0,0,0,0.2)] right-0">
+                    <div className="flex flex-col w-full items-start py-4 pb-0 px-4 text-start text-blue-1 font-medium">
+                        {folders.length > 0 ? (
+                            <div className="w-full flex flex-col gap-2">
+                                <h3 className="w-full text-center text-xl text-blue-1 font-bold"> Salvar </h3>
+                                <div className="flex flex-col max-h-[30vh] overflow-y-scroll [&>div:last-child]:pb-5">
+                                    {
+                                        folders.map((folder) => {
+                                            return (
+                                                <div key={folder.id_pasta} className="py-3 mt-2 rounded-md flex items-center text-lg gap-2">
+                                                    <Link href={'/home/folder/' + folder.id_pasta} className="grow">{folder.nome}</Link>
+                                                    <button onClick={() => postSaveItem(folder)} className="px-3 py-1 h-full bg-blue-1 hover:bg-blue-2 ease-linear duration-100 rounded-md text-white"> Salvar </button>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="py-8 px-4 text-xl font-medium"> Você ainda não possui nenhuma pasta </div>
+                        )}
+                    </div>
+                    <button
+                        className="flex items-center gap-4 shadow-[0_-8px_8px_0px_rgba(0,0,0,0.1)] w-full py-5 px-4 h-fit"
+                        onClick={() => createFolder(true)}
+                    >
+                        <div className="bg-blue-1 h-12 w-12 p-2 rounded-xl flex items-center justify-center">
+                            <Image
+                                alt="Criar pasta"
+                                src={addSVG}
+                                width={100}
+                                height={100}
+                                className="h-full w-full"
+                            />
+                        </div>
+                        <span className="text-xl text-blue-1 font-medium"> Criar pasta </span>
+                    </button>
+                </div>
+            )}
         </div>
     )
 }
