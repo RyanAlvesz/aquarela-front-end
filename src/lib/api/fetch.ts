91922eb0594db0a1315af6df69b5481a1e861a7a
RyanAlvesz <ra546038@gmail.com>
2024-11-26 00:43:08 -0300

    test: localhost url

--- src/lib/api/fetch.ts
+++ src/lib/api/fetch.ts
@@ -2,7 +2,8 @@ export async function fetchWrapper<T = unknown>(
     input: RequestInfo | URL,
     init?: RequestInit | undefined
 ) {
-    const baseUrl: string = 'https://aquarela-nt8n.onrender.com'
+    // const baseUrl: string = 'https://aquarela-nt8n.onrender.com'
+    const baseUrl: string = 'http://localhost:8080'
     const data: Response = await fetch(`${baseUrl}/${input}`, init)
     const result = await data.json()
     return result as T
