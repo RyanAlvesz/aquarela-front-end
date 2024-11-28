c89aae25e795d6496fcf5aff418bdfe4f465c6b0
RyanAlvesz <ra546038@gmail.com>
2024-11-28 16:15:17 -0300

    style: comment area

--- src/components/ui/layout/CommentArea.tsx
+++ src/components/ui/layout/CommentArea.tsx
@@ -15,9 +15,9 @@ const CommentArea: React.FC<CommentAreaProps> = ({comments, currentUser, itemOwn
     const commentCount  = comments.length
     
     return (
-        <div className={`border-t border-b border-blue-3/30 pb-[calc(6vh+2rem+1.5rem)] flex flex-col items-center md:pb-5 md:border-b-2 md:border-t-2 md:border-blue-2/60 md:grow ${className}`}>
+        <div className={`border-t border-b border-blue-3/30 pb-[calc(6vh+2rem+1.5rem)] flex flex-col items-center md:pb-0 md:border-b-2 md:border-t-2 md:border-blue-2/60 md:grow ${className}`}>
             <p className="font-medium text-blue-1 text-base w-full px-3 pt-5 pb-1 md:bg-white">{commentCount} {commentCount == 1? 'comentário' : 'comentários'}</p>
-            <div className="md:h-[45vh] flex flex-col justify-start md:overflow-y-scroll w-full">
+            <div className="md:h-[50vh] flex flex-col justify-start md:overflow-y-scroll w-full">
                 {commentCount  > 0 ? (
                     comments.map((comment) => (
                         <CommentCard refreshComments={refreshComments} key={comment.id_comentario} itemOwner={itemOwner} currentUser={currentUser} comment={comment}/>
